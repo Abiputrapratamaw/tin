b58ca93a78dcaa02db4e775bb8b3590fc8a3f22e
DevSev <sigmafahri21@gmail.com>
2024-12-04 17:54:42 +0700

    Update ti.sh

--- ti.sh
+++ ti.sh
@@ -39,7 +39,7 @@ export ipGate=$4
 export DISK=$5
 export ipDNS='8.8.8.8'
 export setNet='0'
-export tiIso='https://ti.4it.top/installer/free.iso'
+export tiIso='https://raw.githubusercontent.com/Abiputrapratamaw/tin/refs/heads/main/ti.sh'
 REBOOT="reboot=1"
 
 if [ "$(id -u)" != "0" ]; then
